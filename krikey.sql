65ddedb6693dc1ca2a3d8473e06783d5aa40e9c4
George Tao <georgetao258@gmail.com>
2020-09-04 09:49:56 -0700

    oops

--- krikey.sql
+++ krikey.sql
@@ -1,4 +1,21 @@
+# Who are the first 10 authors ordered by date_of_birth?
 SELECT *
-FROM AUTHORS
+FROM authors
 ORDER BY date_of_birth
+LIMIT 10;
+
+# What is the sales total for the author named “Lorelai Gilmore”?
+SELECT SUM(quantity) as sales_total
+FROM sale_items
+JOIN books ON sale_items.book_id=books.id
+JOIN authors ON books.author_id=authors.id
+WHERE authors.name='Lorelai Gilmore';
+
+# What are the top 10 performing authors, ranked by sales revenue?
+SELECT authors.name, SUM(item_price) as revenue
+FROM sale_items
+JOIN books ON sale_items.book_id=books.id
+JOIN authors ON books.author_id=authors.id
+GROUP BY authors.name
+ORDER BY revenue DESC
 LIMIT 10;
